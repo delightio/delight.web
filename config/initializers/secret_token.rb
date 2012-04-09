efd7344cb2fb2ab8e9a9ca723ed54ac10cbff9b3
Thomas Pun <thomas@nowbox.com>
2012-04-09 00:44:41 -0700

    create rails proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelightWeb::Application.config.secret_token = 'cfc909068d44de8838f9a75978837f95d2f7a21897dffadf21194e3a17c8f631aef52c7a9003eb186f19445331b446e533465dd1a9b30568e0680963da1986ec'
