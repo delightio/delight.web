b20fd6e7fb3e66aced28357639eec41a31ff8d35
Thomas Pun <thomas@nowbox.com>
2012-04-10 23:09:26 -0700

    placeholder for better homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 DelightWeb::Application.routes.draw do
+
   resources :app_sessions
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
