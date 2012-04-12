127aacb9f1a4ded9abeaf1b092deadd4fd80fb26
Thomas Pun <thomas@nowbox.com>
2012-04-12 14:09:50 -0700

    /videos only responds to XML request [#14]

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,11 +5,8 @@ class VideosController < ApplicationController
     respond_to do |format|
       if @video.save
         format.xml # TODO: should use custom show.xml. We also don't return the right status code
-        format.json { render json: @video, status: :created }
-        format.html
       else
         format.xml { render xml: @video.errors, status: :bad_request }
-        format.json { render json: @video.errors, status: :bad_request}
       end
     end
   end
