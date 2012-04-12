c3da80ae4aa86458735336828026e52eb5e0bcd4
Thomas Pun <thomas@nowbox.com>
2012-04-12 14:53:39 -0700

    script to debug client console

---
+++ script/debug/client_console.rb
@@ -0,0 +1,43 @@
+require 'awesome_print'
+require 'typhoeus'
+require 'faraday'
+require 'yajl'
+require 'pry'
+
+class ClientConsole
+  LocalHost = "localhost:3000"
+  ProductionHost = "delightweb.herokuapp.com"
+  def initialize
+    @domain = ProductionHost
+    @scheme = "http"
+    @prefix = ""
+    @headers = {}
+    initialize_conn
+  end
+
+  def initialize_conn domain=@domain
+    @domain = domain
+    @conn = Faraday.new "#{@scheme}://#{@domain}/#{@prefix}",
+      :headers => @headers
+  end
+
+  def production!
+    initialize_conn ProductionHost
+  end
+
+  def local!
+    initialize_conn LocalHost
+  end
+
+  def parse_response resp
+    [ resp.status, resp.body, resp.headers ]
+  end
+
+  def method_missing sym, *args, &block
+    super(sym, *args, &block) unless sym =~ /^(?:get|put|post|delete)$/
+
+    parse_response(@conn.send sym, *args)
+  end
+end
+
+ClientConsole.new.pry
