d94e96cb4f271462781ceba23c2c304321e56e5b
Thomas Pun <thomas@nowbox.com>
2012-04-18 00:50:01 -0700

    diff format on validation

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,10 +2,8 @@ class App < ActiveRecord::Base
   belongs_to :account
   has_many :app_sessions
 
-  validates_presence_of :token
-  validates_uniqueness_of :token
-
   before_validation :generate_token, :on => :create
+  validate :token, :presence => true, :uniqueness => true
 
   def generate_token
     self.token = SecureRandom.hex 12
