2ff547bafb77c537d278a630ecd500bec85751b1
Thomas Pun <thomas@nowbox.com>
2012-04-18 01:00:27 -0700

    move FactoryGirl :app to its own file

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,11 +5,6 @@ FactoryGirl.define do
     #a.association :administrator
   end
 
-  factory :app do |a|
-    a.name 'Nowbox'
-    a.association :account
-  end
-
   factory :app_session do |a|
     a.app_version '1.4'
     a.delight_version '0.1'
