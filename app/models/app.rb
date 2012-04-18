30c001d2e8d7ccbd862c9d69e8c76bbf357e482d
Michael Au-Yeung <michael@primitus.com>
2012-04-18 15:49:47 +0800

    [#34] rewrite apps controller, view, and spec

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,12 +2,27 @@ class App < ActiveRecord::Base
   belongs_to :account
   has_many :app_sessions
 
+  has_many :permissions
+  has_many :viewers, :through => :permissions 
+
   validates_presence_of :token
   validates_uniqueness_of :token
 
   before_validation :generate_token, :on => :create
 
+  module Scopes
+    def administered_by(user) 
+      joins(:account).where(:accounts => { :administrator_id => user.id }) 
+    end 
+
+    def viewable_by(user)
+      joins(:permissions).where(:permissions => { :viewer_id => user.id })
+    end
+  end
+  extend Scopes
+
   def generate_token
     self.token = SecureRandom.hex 12
   end
+
 end
