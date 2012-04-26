e4c7d352a889e00c6bdd07f45aeb7f2f77807a07
Michael Au-Yeung <michael@primitus.com>
2012-04-26 15:23:06 +0800

    [#72] Update admin user checking route logic

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -13,10 +13,6 @@ describe ApplicationController do
     end
   end
 
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-  end
-
   let(:user) { FactoryGirl.create(:user, :signup_step => 1) }
   let(:viewer) { FactoryGirl.create(:viewer, :signup_step => 1) }
   let(:admin) { FactoryGirl.create(:administrator, :signup_step => 1) }
@@ -33,18 +29,37 @@ describe ApplicationController do
       before(:each) do
         sign_in(admin)
       end
-      it "should redirect to apps page if there is account" do
+      it "should redirect to apps page if there is account and app and signup complete" do
         admin.signup_step = 2
         admin.save!
-        admin.create_account(:name => "test account")
+        account = admin.create_account(:name => "test account")
+        account.apps.create(:name => "test app")
         get 'index'
         response.should be_success # no additional redirect
       end
       it "should redirect user to force account create page if no account" do
+        admin.signup_step = 2
+        admin.save!
+        get 'index'
+        response.should redirect_to user_signup_info_edit_path(admin)
+      end
+      it "should redirect user to force account create page if no app" do
+        admin.signup_step = 2
+        admin.save!
+        account = admin.create_account(:name => "test account")
+        get 'index'
+        response.should redirect_to user_signup_info_edit_path(admin)
+      end
+      it "should redirect user to force account create page if no app" do
+        get 'index'
+        response.should redirect_to user_signup_info_edit_path(admin)
+      end
+      it "should redirect user to force account create page if signup not completed" do
         get 'index'
         response.should redirect_to user_signup_info_edit_path(admin)
       end
     end
+
     describe "signed in as viewer" do
       before(:each) do
         sign_in(viewer)
@@ -54,6 +69,7 @@ describe ApplicationController do
         response.should redirect_to edit_user_path(viewer)
       end
     end
+
     describe "signed in as user" do
       before(:each) do
         sign_in(user)
