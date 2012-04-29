8126937eef9ec47bcf75193a886cf9a4cd0fb660
Thomas Pun <thomas@nowbox.com>
2012-04-29 13:46:29 +0800

    updated email template

--- app/workers/email_notification/app_recording_completion.rb
+++ app/workers/email_notification/app_recording_completion.rb
@@ -8,7 +8,7 @@ class AppRecordingCompletion < EmailNotification
       "[delight.io] New sessions recorded for '#{app.name}'",
       "Please visit #{app_url(app.id)} to watch your newly recorded sessions.\n\n"\
       "Thank you for using our service,\n"\
-      "   Team delight.io\n\n"\
+      "   The Delight Team\n\n"\
       "Follow us on Twitter: http://twitter.com/delightio"
   end
 
