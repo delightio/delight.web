527368c352bab42942216f5e9a1d7801af19304b
Michael Au-Yeung <michael@primitus.com>
2012-05-10 16:23:13 +0800

    [#111] duration and date slider always active

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -63,12 +63,9 @@ class AppsController < ApplicationController
     duration_min = params[:'duration-min'] || @default_duration_min
     duration_max = params[:'duration-max'] || @default_duration_max
 
-    if (params[:filter_duration])
-      @recorded_sessions = @recorded_sessions.duration_between(duration_min, duration_max)
-    end
-    if (params[:filter_date])
-      @recorded_sessions = @recorded_sessions.date_between(date_min, date_max)
-    end
+    @recorded_sessions = @recorded_sessions.duration_between(duration_min, duration_max)
+    @recorded_sessions = @recorded_sessions.date_between(date_min, date_max)
+
     @recorded_sessions = @recorded_sessions.where(:app_version => versions)
     if params[:favorite] == "1"
       @recorded_sessions = @recorded_sessions.favorite_of(current_user)
