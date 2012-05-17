07c1789c654ecd4b587018c470b06c8121cdb62d
Thomas Pun <thomas@nowbox.com>
2012-05-17 16:43:40 -0700

    [#97] more update

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -5,6 +5,6 @@ class SystemMailer < ActionMailer::Base
     @user = user
     @invitation = invitation
     @invitation_url = url_for(:controller => "invitations", :action => "show", :token => @invitation.token, :id => @invitation.id, :email => @invitation.email)
-    mail(:to => invitation.email, :subject => 'You are invited to delighio')
+    mail(:to => invitation.email, :subject => "#{@user.nickname} invited you to delight.io")
   end
 end
