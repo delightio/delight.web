659758a04dcae25440c465776368db90414ec2fe
Thomas Pun <thomas@nowbox.com>
2012-05-18 15:25:36 -0700

    [#99] use Redis hash to cache federated session credentials

--- app/models/s3_storage.rb
+++ app/models/s3_storage.rb
@@ -1,24 +1,29 @@
 class S3Storage
 
-  SESSION_DURATION = 3600 # 1 hour
-  SESSION_RENEW_INTERVAL = 50.minutes # a bit earlier than the session expiration
-
   attr_reader :filename, :bucket_name
   def initialize filename, bucket_name=ENV['S3_UPLOAD_BUCKET']
     @filename = filename
     @bucket_name = bucket_name
+    @credentials = CachedHash.new "#{self.class}_Credentials",
+                                  1.hours - 1.minutes # give it some buffer
+  end
+
+  def self.session
+    policy = AWS::STS::Policy.new
+    policy.allow(:actions => :any, :resource => :any)
+    AWS::STS.new.new_federated_session("session",
+                                       :policy => policy,
+                                       :duration => 1.hours)
+  end
+
+  def cached_credentials
+    return @credentials.get unless @credentials.expired?
+    @credentials.set self.class.session.credentials
   end
 
   def presigned_bucket
-    return $s3_presigned_bucket if $s3_presigned_bucket and $s3_last_session and $s3_last_session > SESSION_RENEW_INTERVAL.ago
-
-    policy = AWS::STS::Policy.new
-    policy.allow(:actions => :any, :resource => :any)
-    session = AWS::STS.new.new_federated_session("session", :policy => policy, :duration => SESSION_DURATION)
-    s3 = AWS::S3.new(session.credentials)
-
-    $s3_last_session = Time.now
-    $s3_presigned_bucket = s3.buckets[@bucket_name]
+    s3 = AWS::S3.new cached_credentials
+    s3.buckets[@bucket_name]
   end
 
   def presigned_object
@@ -49,5 +54,26 @@ class S3Storage
     local_file.pos = 0
     presigned_object.write local_file.read
     puts "#{local_file} was uploaded to S3:#{@bucket_name}/#{@filename} in #{Time.now-start} s."
+  end
+end
+
+class CachedHash
+  def initialize key, ttl
+    @key = key
+    @ttl = ttl
+  end
+
+  def expired?
+    REDIS.ttl(@key) == -1
+  end
+
+  def set new_hash
+    REDIS.hmset @key, *new_hash.to_a.flatten
+    REDIS.expire @key, @ttl
+    get
+  end
+
+  def get
+    REDIS.hgetall @key
   end
 end
