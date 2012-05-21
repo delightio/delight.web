5d69de5822c936d508c5a39f8f25c825a9467416
Michael Au-Yeung <michael@primitus.com>
2012-05-21 14:31:33 +0800

    [#135] log user sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   #devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :trackable, :validatable
   #devise :database_authenticatable, :registerable, :omniauthable
-  devise :omniauthable
+  devise :omniauthable, :trackable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :twitter_id, :github_id, :nickname, :image_url, :signup_step, :type
