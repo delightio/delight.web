22224375d7174085b2a8b91a882c41e9b2f2867d
Michael Au-Yeung <michael@primitus.com>
2012-05-22 15:40:13 +0800

    [#97] invitation flow disable form while sending

---
+++ app/assets/javascripts/shared.js
@@ -0,0 +1,12 @@
+    function clearErrorMessages() { 
+      var $container = $('#error-messages-container');
+      $container.empty();
+    } 
+    function addErrorMessage(type, msg) { 
+      var $container = $('#error-messages-container');
+      $container.append('<div class="' + type + ' fade in" data-alert="alert"><a class="close" data-dismiss="alert">x</a>' + msg + '</div>');
+    } 
+    function displayErrorMessage(type, msg) { 
+      clearErrorMessages();
+      addErrorMessage(type, msg); 
+    } 
