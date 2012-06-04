8a29631cd8409eb1dfc613657b03091cf5eff292
Michael Au-Yeung <michael@primitus.com>
2012-06-04 12:48:30 +0800

    [#182] display time in english words

--- app/helpers/app_sessions_helper.rb
+++ app/helpers/app_sessions_helper.rb
@@ -1,2 +1,10 @@
 module AppSessionsHelper
+  def session_time_in_words(time)
+    if time < Time.now
+      suffix = ' ago'
+    else
+      suffix = ''
+    end
+    return distance_of_time_in_words_to_now(time) + suffix
+  end
 end
