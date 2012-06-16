b269b0c4c566e73f0e4f4cc990ef7dd747e71069
Thomas Pun <thomas@nowbox.com>
2012-06-15 18:54:25 -0700

    changed opengl frame rate back to 2 fps

--- spec/models/opengl_app_session_spec.rb
+++ spec/models/opengl_app_session_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe OpenglAppSession do
   subject { FactoryGirl.create :opengl_app_session }
-  its(:maximum_frame_rate) { should == 5 }
+  its(:maximum_frame_rate) { should == 2 }
 end
