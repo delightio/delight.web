6af143ea36d569c2fdcbc8e8b9e7521983620934
Victor Lam [Primitus] <victor@primitus.com>
2012-07-04 16:14:27 +0800

    [#230] rollback player UI related changes. moved changes to another branch player_ui_update_LH241

--- app/assets/javascripts/jquery.acornmediaplayerparallel.js
+++ app/assets/javascripts/jquery.acornmediaplayerparallel.js
@@ -91,14 +91,6 @@
 				captionsChoose: 'Choose caption',
 				transcript: 'Transcript',
 				transcriptTitle: 'Show transcript'
-				rotateLeft90: '-90';
-				rotateLeft90Title: 'Rotate -90';
-				rotate0: '0';
-				rotate0Title: 'Original';
-				rotate90: '90';
-				rotate90Title: 'Rotate 90';
-				rotate180: '180';
-				rotate180Title: 'Rotate 180';
 			};
 
 			// main wrapper element
@@ -136,18 +128,14 @@
 								'<button class="acorn-play-button" title="' + text.playTitle + '" aria-controls="' + acorn.id + '">' + text.play + '</button>' +
 								'<input type="range" class="acorn-seek-slider" title="' + text.seekTitle + '" value="0" min="0" max="150" step="0.1" aria-controls="' + acorn.id + '"/>' +
 								'<span class="acorn-timer">00:00</span>' +
-								// '<div class="acorn-volume-box">' +
-								// 	'<button class="acorn-volume-button" title="' + text.mute + '" aria-controls="' + acorn.id + '">' + text.mute + '</button>' +
-								// 	'<input type="range" class="acorn-volume-slider" title="' + text.volumeTitle + '" value="1" min="0" max="1" step="0.05" aria-controls="' + acorn.id + '"/>' +
-								// '</div>' +
+								'<div class="acorn-volume-box">' +
+									'<button class="acorn-volume-button" title="' + text.mute + '" aria-controls="' + acorn.id + '">' + text.mute + '</button>' +
+									'<input type="range" class="acorn-volume-slider" title="' + text.volumeTitle + '" value="1" min="0" max="1" step="0.05" aria-controls="' + acorn.id + '"/>' +
+								'</div>' +
 								// fullscreenBtnMarkup +
-								// '<button class="acorn-caption-button" title="' + text.captionsTitle + '"  aria-controls="' + acorn.id + '">' + text.captions + '</button>' +
-								// '<div class="acorn-caption-selector"></div>' +
-								// '<button class="acorn-transcript-button" title="' + text.transcriptTitle + '">' + text.transcript + '</button>' +
-								'<button class="acorn-rotate-button" title="' + text.rotate180Title + '" id="rotate--90">' + text.rotate180 + '</button>' +
-								'<button class="acorn-rotate-button" title="' + text.rotate90Title + '" id="rotate--90">' + text.rotate90 + '</button>' +
-								'<button class="acorn-rotate-button" title="' + text.rotate0Title + '" id="rotate--90">' + text.rotate0 + '</button>' +
-								'<button class="acorn-rotate-button" title="' + text.rotateLeft90Title + '" id="rotate--90">' + text.rotateLeft90 + '</button>' +
+								'<button class="acorn-caption-button" title="' + text.captionsTitle + '"  aria-controls="' + acorn.id + '">' + text.captions + '</button>' +
+								'<div class="acorn-caption-selector"></div>' +
+								'<button class="acorn-transcript-button" title="' + text.transcriptTitle + '">' + text.transcript + '</button>' +
 							'</div>';
 
 			var captionMarkup = '<div class="acorn-caption"></div>';
