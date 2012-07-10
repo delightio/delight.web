0a619c549d75c7411b3061eb797495f1c91f47bc
Thomas Pun <thomas@nowbox.com>
2012-07-10 02:36:50 -0700

    [#196] auto rotate to major orientation

--- app/models/orientation_track.rb
+++ app/models/orientation_track.rb
@@ -1,5 +1,16 @@
+require 'json'
+
 class OrientationTrack < Track
   def file_extension
     'plist'
+  end
+
+  def rotation duration
+    return @rotation if @rotation
+
+    file = download
+    out = `majororientation -f #{file.path} -d #{duration}`
+    json = JSON.parse out
+    @rotation = json['rotation']
   end
 end
