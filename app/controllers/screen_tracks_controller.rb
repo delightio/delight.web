8f3ef34ec54866aa2c5c87b48867b847f364a243
Thomas Pun <thomas@nowbox.com>
2012-07-16 11:40:18 -0700

    refactoring track controller

--- app/controllers/screen_tracks_controller.rb
+++ app/controllers/screen_tracks_controller.rb
@@ -2,8 +2,4 @@ class ScreenTracksController < TracksController
   def model_class
     ScreenTrack
   end
-
-  def model_param_key
-    :screen_track
-  end
 end
--- app/controllers/touch_tracks_controller.rb
+++ app/controllers/touch_tracks_controller.rb
@@ -2,8 +2,4 @@ class TouchTracksController < TracksController
   def model_class
     TouchTrack
   end
-
-  def model_param_key
-    :touch_track
-  end
 end
