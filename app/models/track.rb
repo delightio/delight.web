6d9c4141b6374fa9009c27883851791d743a0caf
Thomas Pun <thomas@pun.io>
2013-01-21 01:52:57 +0900

    Only allow one type of track to be created per session. Fixed #1

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,19 @@
+class UniqueTrackValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    other_tracks_type = Track.where(:app_session_id => value).
+                          select("type").map &:type
+    if (other_tracks_type.include? record.type)
+      record.errors[:base] << "App Session #{value} already has uploaded #{record.type}."
+    end
+  end
+end
+
+
 class Track < ActiveRecord::Base
+  include ActiveModel::Validations
 
   belongs_to :app_session, :counter_cache => true
-  validates :app_session_id, :presence => true
+  validates :app_session_id, :presence => true, :unique_track => true, :on => :create
 
   after_create { |t| app_session.track_uploaded self }
 
@@ -30,3 +42,4 @@ class Track < ActiveRecord::Base
     storage.download app_session.working_directory
   end
 end
+
