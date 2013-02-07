5a74a37272a3edaf032e9af25e6f6c684248df55
paranoiase Kang <paranoiase@gmail.com>
2013-02-07 10:12:10 +0900

    add logs for event track parsing

--- app/workers/event_track_parsing.rb
+++ app/workers/event_track_parsing.rb
@@ -3,6 +3,9 @@ class EventTrackParsing
   @queue = :event_track
 
   def self.perform app_session_id
+    start = Time.now
+    puts "AppSession[#{app_session_id}] is processing..."
+
     app_session = AppSession.find(app_session_id)
     event = app_session.event_track.download
 
@@ -13,9 +16,12 @@ class EventTrackParsing
         app_session.events.create!(name: data["name"])
       end
     end
+
+    puts "AppSession[#{app_session_id}]: done processing in #{Time.now-start} s."
   end
 
   def self.enqueue app_session_id
     Resque.enqueue self, app_session_id
+    puts "AppSession[#{app_session_id}] is enqueued for EventTrackParsing"
   end
 end
