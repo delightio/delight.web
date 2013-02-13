893089209275900b184d620304fba10f38db1f58
paranoiase Kang <paranoiase@gmail.com>
2013-02-13 17:30:32 +0900

    edit funnel

--- app/controllers/funnels_controller.rb
+++ app/controllers/funnels_controller.rb
@@ -8,7 +8,17 @@ class FunnelsController < ApplicationController
   def create
     @funnel = @app.funnels.build(params[:funnel])
     @funnel.save!
-    redirect_to @app
+    redirect_to @app, notice: 'Funnel was successfully created.'
+  end
+
+  def edit
+    @funnel = @app.funnels.find(params[:id])
+  end
+
+  def update
+    @funnel = @app.funnels.find(params[:id])
+    @funnel.update_attributes!(params[:funnel])
+    redirect_to @app, notice: 'Funnel was successfully updated.'
   end
 
 private
