d8f49720f1a75151653ac6cc19103ab645ac8417
Thomas Pun <thomas@pun.io>
2013-03-05 16:58:52 +0800

    Hack to enable authenication by token for /tracks  #16

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,22 +1,24 @@
 class TracksController < ApplicationController
-  before_filter :authenticate!
-
-  def authenticate!
-    if request.format.html?
-      authenticate_user!
-    else
-      authenticate_by_token!
-    end
-  end
-
   def create
     if params[model_param_key].nil?
       render xml: "Missing #{model_param_key} key", status: :bad_request
       return
     end
 
+    token = get_token
+    if token.nil?
+      render xml: "Missing HTTP_X_NB_AUTHTOKEN HTTP header", status: :bad_request
+      return
+    end
+
     if params[model_param_key][:app_session_id].nil?
       render xml: "Missing #{model_param_key}.app_session_id key", status: :bad_request
+      return
+    end
+
+    @app_session = AppSession.find(params[model_param_key][:app_session_id])
+    if token != @app_session.app.token
+      render xml: "Token mismatch", status: :bad_request
       return
     end
 
@@ -33,9 +35,15 @@ class TracksController < ApplicationController
   end
 
   def show
+    unless request.format.json?
+      authenticate_user!
+    else
+      authenticated = authenticate_by_token
+      return unless authenticated
+    end
+
     @track = model_class.find(params[:id]) # throws exception if not found
     app = @track.app_session.app
-
     if app.administered_by?(current_user) or app.viewable_by?(current_user)
       respond_to do |format|
         format.html { render :layout => 'empty' }
@@ -52,33 +60,22 @@ class TracksController < ApplicationController
     end
   end
 
-  def authenticate_by_token!
+  # TODO: Should put ApplicationController and use before_filter to trigger
+  def authenticate_by_token
     token = get_token
     if token.nil?
-      render xml: "Missing HTTP_X_NB_AUTHTOKEN HTTP header", status: :bad_request
-      return
+      render json: "Missing HTTP_X_NB_AUTHTOKEN HTTP header", status: :bad_request
+      return false
     end
 
     # We verify by two ways:
     # 1. if app session id is present, does the app token match the associated app?
     # 2. does the current track belongs to the app session which the app token points to?
-    true_token = nil
-    if params[model_param_key].nil? || params[model_param_key][:app_session_id].nil?
-      if params[:id].nil?
-        render xml: "Missing #{model_param_key}.app_session_id key", status: :bad_request
-        return
-      end
-      track = model_class.find params[:id]
-      true_token = track.app_session.app.token
-
-    else
-      app_session = AppSession.find(params[model_param_key][:app_session_id])
-      true_token = app_session.app.token
-    end
+    true_token = Track.find(params[:id]).app_session.app.token
 
     if token != true_token
-      render xml: "Token mismatch", status: :bad_request
-      return
+      render json: "Token mismatch", status: :bad_request
+      return false
     end
 
     true
