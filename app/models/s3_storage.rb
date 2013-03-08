d15d7ce41c583b75a0b5fc1e7ac2f29a39644dc1
Thomas Pun <thomas@pun.io>
2013-03-08 17:14:09 +0800

    Set content-type when generating presigned uri. This is a quick workaround for #42. We should probably set the mime type when uploading instead.

--- app/models/s3_storage.rb
+++ app/models/s3_storage.rb
@@ -30,11 +30,15 @@ class S3Storage
   end
 
   def presigned_write_uri
-    presigned_object.url_for :write
+    presigned_object.url_for :write, response_content_type: content_types.first.to_s
   end
 
   def presigned_read_uri
-    presigned_object.url_for :read
+    presigned_object.url_for :read, response_content_type: content_types.first.to_s
+  end
+
+  def content_types
+    MIME::Types.type_for @filename
   end
 
   def download local_directory
