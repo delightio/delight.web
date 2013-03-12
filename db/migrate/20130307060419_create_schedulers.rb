8144aa892f11d6e49387cd63b744897c15a74d54
Thomas Pun <thomas@pun.io>
2013-03-12 15:08:52 +0800

    move scheduler pre population to later migration

--- db/migrate/20130307060419_create_schedulers.rb
+++ db/migrate/20130307060419_create_schedulers.rb
@@ -9,28 +9,5 @@ class CreateSchedulers < ActiveRecord::Migration
 
       t.timestamps
     end
-
-    App.find_each do |app|
-      wifi_only = app.settings["uploading_on_wifi_only"]
-      recordings = app.settings["recordings"].to_i
-      recording_state = app.settings["recording_state"]
-
-      app.scheduler = Scheduler.create app_id: app.id
-      app.save
-      app.reload
-
-      app.schedule_recordings app.settings["recordings"].to_i
-      if wifi_only == "true"
-        app.set_uploading_on_wifi_only true
-      else
-        app.set_uploading_on_wifi_only false
-      end
-      if recording_state == "recording"
-        app.resume_recording
-      else
-        app.pause_recording
-      end
-    end
-
   end
 end
