256078afd50c2fcae5d706fbdac0c101da9142e0
Thomas Pun <thomas@pun.io>
2013-03-22 23:09:16 +0800

    partner account subscribes to unlimited plan

--- app/models/partner_account.rb
+++ app/models/partner_account.rb
@@ -2,6 +2,6 @@ class PartnerAccount < Account
   after_create :subscribe_to_unlimited_plan
 
   def subscribe_to_unlimited_plan
-    subscribe 'unlimited'
+    subscribe UnlimitedPlan.id
   end
 end
