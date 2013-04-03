8c8a5b771e8f3fb0bdc6640ece0024ae8c823d86
Vinci <vinci.cat@gmail.com>
2013-04-03 19:10:25 +0800

    Message Display Enhancement
    WIP #37

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -4,7 +4,7 @@
     } 
     function addErrorMessage(type, msg) { 
       var $container = $('#error-messages-container');
-      $container.append('<div class="alert alert-' + type + ' fade in" data-alert="alert"><a class="close" data-dismiss="alert" href="#">x</a>' + msg + '</div>');
+      $container.append('<div class="alert alert-' + type + ' fade in" data-alert="alert"><a class="close" data-dismiss="alert" href="#">×</a>' + msg + '</div>');
     } 
     function displayErrorMessage(type, msg) { 
       clearErrorMessages();
