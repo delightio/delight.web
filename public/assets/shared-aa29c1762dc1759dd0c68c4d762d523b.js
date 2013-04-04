d44d1779c66693a31bccb1d528c32ff590a725b3
Thomas Pun <thomas@pun.io>
2013-04-04 11:54:42 +0800

    precompiled assets

---
+++ public/assets/shared-aa29c1762dc1759dd0c68c4d762d523b.js
@@ -0,0 +1,1 @@
+function clearErrorMessages(){var e=$("#error-messages-container");e.empty()}function addErrorMessage(e,t){var n=$("#error-messages-container");n.append('<div class="alert alert-'+e+' fade in" data-alert="alert"><a class="close" data-dismiss="alert" href="#">×</a>'+t+"</div>")}function displayErrorMessage(e,t){clearErrorMessages(),addErrorMessage(e,t)};
