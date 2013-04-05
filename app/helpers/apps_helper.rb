8d332a758710cbc4fa2a56bfab861caf3667ff18
Thomas Pun <thomas@pun.io>
2013-04-05 19:46:21 +0800

    different bar color and call to action depending on percentage left

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -5,4 +5,23 @@ module AppsHelper
     truncated[-3..-1] = "..."
     truncated
   end
+
+  def progress_bar_class percentage
+    if percentage < 70
+      return 'bar'
+    elsif percentage < 90
+      return 'bar bar-warning'
+    else
+      return 'bar bar-danger'
+    end
+  end
+
+  def subscription_call_to_action percentage
+    percentage = [percentage, 100].min
+    if percentage < 70
+      "#{pluralize(@subscription.remaining_hours, 'hour')} for next #{pluralize(@subscription.days_till_expired, 'day')}"
+    else percentage <= 100
+      "#{100-percentage}% left. Upgrade now"
+    end
+  end
 end
